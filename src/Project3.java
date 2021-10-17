b1db13ede8e85231f5dcb0dc7623cfd205e9b213
shitalm277 <shitalmehta277@gmail.com>
2021-10-17 16:04:34 +0100

    Project3

---
+++ src/Project3.java
@@ -0,0 +1,7 @@
+public class Project3 {
+    public static void main(String[] args){
+    System.out.println("i like git");
+    System.out.println("i like git");
+    System.out.println("i like git");
+    }
+}
